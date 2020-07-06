58f655ca29e87fe16b5c5e144d88e91f179272c7
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-07-05 21:02:42 -0700

    Adding persistent store

--- fend/src/redux/index.ts
+++ fend/src/redux/index.ts
@@ -1,15 +1,19 @@
+import { connectRouter, RouterState } from "connected-react-router";
+import { History } from "history";
 import { combineReducers } from "redux";
 import { Dataset } from "../../../shared/Dataset";
 import { datasetReducer, metaDataReducer } from "./reducers";
 import { MetaData } from "./types";
 
-export default function createRootReducer() {
+export default function createRootReducer(history: History) {
   return combineReducers({
     dataset: datasetReducer,
     metaData: metaDataReducer,
+    router: connectRouter(history),
   });
 }
 export interface RootState {
   dataset: Dataset;
   metaData: MetaData;
+  router: RouterState;
 }
--- fend/src/redux/store.ts
+++ fend/src/redux/store.ts
@@ -1,6 +1,32 @@
-import { createStore } from "redux";
-import rootReducer from "./index";
+import { routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+import createRootReducer from "./index";
 
-const store = createStore(rootReducer()); //TODO: Find way to extend to combineReducers
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["dataset", "metadata"], // Top level store branches to persist
+};
+
+export const history = createBrowserHistory();
+
+const middleware = [thunk, routerMiddleware(history)];
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  createRootReducer(history)
+);
+
+export const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export const persistor = persistStore(store);
 
 export default store;
