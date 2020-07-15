0b0f2482c471904dfef044907a4e7b4e3c223fca
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-07-15 11:28:09 -0700

    BEND - Moving query params to body of request

--- bend/src/server.ts
+++ bend/src/server.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import express, { Request, Response } from 'express'
+import { SearchItem } from '../../shared/Dataset'
 import {
   getDatasetByName,
   getDatasetNames
@@ -30,27 +31,36 @@ app.get('/dataset/:datasetName', (req: Request, res: Response) =>
   getDatasetByName(res, req.params.datasetName)
 )
 app.post(
-  '/target/:datasetName/:artifactType/:artifactId/:limit',
-  async (req: Request, res: Response) =>
-    res.send(
-      await makePredictionsForArtifact(
-        req.params.datasetName,
-        req.params.artifactType,
-        req.params.artifactId,
-        req.body.query,
-        req.params.limit
-      )
-    )
-)
-app.post('/source/:datasetName/:limit', async (req: Request, res: Response) =>
-  res.send(
-    await searchForArtifact(
-      req.params.datasetName,
+  '/target',
+  async (req: Request, res: Response, next) => {
+    // TODO: Check for required fields used below
+    const dataPromise = makePredictionsForArtifact(
+      req.body.datasetName,
+      req.body.sourceType,
+      req.body.sourceId,
       req.body.query,
-      req.params.limit
+      req.body.limit
     )
-  )
-)
+    handleError(res, dataPromise)
+  })
+
+app.post('/source',
+  async (req: Request, res: Response, next) => {
+    // TODO: Check for required fields used below
+    const data = searchForArtifact(
+      req.body.datasetName,
+      req.body.query,
+      req.body.limit
+    )
+    handleError<SearchItem[]>(res, data)
+  })
+
+function handleError <T> (res: Response, data: Promise<T>) {
+  data.then(data => res.send(data)).catch(e => { // convention is to reject with error
+    res.status(400)
+    res.send(e)
+  })
+}
 
 /*
  * START SERVER
