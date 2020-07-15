7ccdd60a4644a97d3c3a4ca3db355fe0d23f1b7a
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-07-15 11:31:43 -0700

    Adding syntax highlighting for classes

--- fend/src/components/target/ArtifactDisplay.tsx
+++ fend/src/components/target/ArtifactDisplay.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import styled from "styled-components";
 import { Artifact } from "../../../../shared/Dataset";
 import { PAGE_NAV_MARGIN_TOP } from "../nav/PageTitle";
@@ -8,11 +10,23 @@ interface ArtifactDisplayProps {
 }
 
 export default function ArtifactDisplay(props: ArtifactDisplayProps) {
-  return (
-    <ArtifactContainer>
-      <ArtifactBody>{props.artifact.body}</ArtifactBody>
-    </ArtifactContainer>
-  );
+  let body;
+
+  if (props.artifact.type === "Classes") {
+    body = (
+      <SyntaxHighlighter
+        language="java" //TODO: Dynamically set this field
+        showLineNumbers
+        style={docco}
+        customStyle={{ background: "none" }}
+      >
+        {props.artifact.body}
+      </SyntaxHighlighter>
+    );
+  } else {
+    body = <ArtifactBody>{props.artifact.body}</ArtifactBody>;
+  }
+  return <ArtifactContainer>{body}</ArtifactContainer>;
 }
 
 const CONTAINER_TOP_PADDING = 100;
