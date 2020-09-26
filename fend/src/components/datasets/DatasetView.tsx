cca38cfee77d01146b3120b1e61f3dbbd6452e93
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-09-25 23:51:31 -0700

    added border at pane middle

--- fend/src/components/datasets/DatasetView.tsx
+++ fend/src/components/datasets/DatasetView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DatasetViewer() {
   return (
-    <div className="flexRowCentered sizeFull debug">
+    <div className="flexRowCentered sizeFull">
       <div className="centeredColumn heightFull">
         <h2 className="textAlignCenter">[MARO's WORK]</h2>
       </div>
--- fend/src/components/viewer/controller/Viewer.tsx
+++ fend/src/components/viewer/controller/Viewer.tsx
@@ -1,11 +1,12 @@
 
 import React from 'react';
 import SplitPane from "react-split-pane";
-import { WordModal } from "../accordion/TraceExplanation";
+import { secondaryColor } from '../../../styles/theme';
+import { WordModal } from '../accordion/TraceExplanation';
 import LoadingBar from './LoadingPage';
 
 interface ViewerProps {
-  leftPanel: JSX.Element | null
+  leftPanel: JSX.Element
   rightPanel: JSX.Element
   loading: boolean
   modalOpen: boolean
@@ -14,11 +15,14 @@ interface ViewerProps {
 export function Viewer(props: ViewerProps) {
   const { leftPanel, rightPanel, modalOpen, loading } = props
   const loadingBar = <LoadingBar open={loading} />
+
   return (
     <div className="flexColumn heightFull overflowYHidden">
-      <SplitPane split="vertical">
-        {leftPanel}
-        {rightPanel}
+      <SplitPane
+        split="vertical"
+      >
+        <div className="heightFull" style={{ borderRight: `1px solid ${secondaryColor}` }}>{leftPanel}</div>
+        <div className="heightFull" style={{ borderLeft: `1px solid ${secondaryColor}` }}>{rightPanel}</div>
       </SplitPane>
       {loadingBar}
       <WordModal open={modalOpen} />
