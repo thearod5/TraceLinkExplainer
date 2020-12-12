eaa4b8ffd568a195f2005f358799b3c6d512bd12
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-12-12 14:31:50 -0800

    removing and changing secret key

--- bend/src/server/settings.py
+++ bend/src/server/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import dotenv
+
+dotenv.load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sqmhyl=d(un!!wm&q@)8dn!*nto0ci7q%7p9l*-29xmpw*bo09'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
